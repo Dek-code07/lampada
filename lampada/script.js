9216244419d3b2e85b61c9300c6a5bba27c68607
Cauê Souza de Lima <103838608+Dek-code07@users.noreply.github.com>
2023-01-23 13:53:04 -0300

    Add files via upload

---
+++ lampada/script.js
@@ -0,0 +1,20 @@
+function Myfunction(){
+
+    document.getElementById('myImage').src='image/Captura de tela 2023-01-23 104140.png';
+
+    var audio = new Audio('desligando.mp3')
+    audio.play();
+    audio.currentTime=0;
+
+}
+
+function Myfunction2(){
+
+    document.getElementById('myImage').src='image/lampada ligada.png';
+
+    var audio = new Audio('y2matecom-som-de-interruptor-de-luz-efeito-sono_LwQzl23J.mp3');
+    audio.play();
+    audio.currentTime=0;
+
+
+}
